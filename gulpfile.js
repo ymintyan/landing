d9c35a70a81de28e2bede81afebb63b698643f89
ymintyan <9zhenya4@gmail.com>
2017-02-12 22:39:44 +0200

    commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+ 
+gulp.task('sass', function(){ 
+    return gulp.src('scss/*.scss') 
+        .pipe(sass()) 
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) 
+        .pipe(gulp.dest('css/')) 
+    
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch('scss/*.scss', ['sass']);
+});
+
+ 
+gulp.task('default', ['sass', 'watch']);
